52a51ee3431f3b92619d1bee501ed39383c7ffe7
KevinLin <kevinlin0638@gmail.com>
2017-12-07 19:04:09 +0800

    作業完成開始ExtraWork

--- GUIHandler.py
+++ GUIHandler.py
@@ -1,5 +1,8 @@
+# -*-coding:utf-8-*-
 import tkinter as tk
-
+import tkinter.messagebox as msb
+import Generator as sg
+import numpy as np
 
 class GUIHandler:
     def __init__(self):
@@ -8,33 +11,115 @@ class GUIHandler:
         self.mtk.resizable(0, 0)
         self.mtk.title('音訊產生器')
         self.ini_widges()
+        self.count = 0
+        self.generator = sg.SoundGenerator(16200, 1)
 
     def ini_widges(self):
-        self.filename_inp = tk.Entry(self.mtk).grid(row=1, column=0, padx=10)
+        self.filename_inp = tk.Entry(self.mtk)
+        self.filename_inp.grid(row=1, column=0, padx=10)
 
         self.outputfn_txt = tk.Label(self.mtk, text='輸出檔名 : ').grid(row=0, column=0)
 
         self.sample_rate = tk.Entry(self.mtk).grid(row=1, column=2, padx=10)
 
-        self.outputfn_txt = tk.Label(self.mtk, text='取樣率 : ').grid(row=0, column=2)
+        self.outputfn_txt = tk.Label(self.mtk, text='取樣率(預設:16200) : ').grid(row=0, column=2)
 
-        self.btn_output = tk.Button(self.mtk, text='作業輸出').grid(row=1, column=1)
+        self.btn_output = tk.Button(self.mtk, text='作業輸出(小蜜蜂雙聲道)', command=self.homeworkout)
+        self.btn_output.grid(row=1, column=1)
 
 
         self.outputfn_txt = tk.Label(self.mtk, text='使用教學 : 請輸入 1 ~ 7(Do ~ Si)  0 為休止符 並輸入拍子數').grid(row=2, column=1, pady=40)
 
         self.outputfn_txt = tk.Label(self.mtk, text='音符(0~7) : ').grid(row=3, column=0, pady=40)
-        self.chord = tk.Entry(self.mtk).grid(row=4, column=0, padx=10)
+
+        self.chord = tk.Entry(self.mtk)
+        self.chord.grid(row=4, column=0, padx=10)
 
         self.outputfn_txt = tk.Label(self.mtk, text='拍數 : ').grid(row=3, column=1)
-        self.sec = tk.Entry(self.mtk).grid(row=4, column=1, padx=10)
+        self.sec = tk.Entry(self.mtk)
+        self.sec.grid(row=4, column=1, padx=10)
 
-        self.btn_delete = tk.Button(self.mtk, text='刪除一個').grid(row=4, column=2, padx=50, sticky='E')
-        self.btn_sumit = tk.Button(self.mtk, text='增加').grid(row=4, column=2, sticky='W')
+        self.btn_delete = tk.Button(self.mtk, text='刪除一個', command=self.btn_delete_handler).grid(row=4, column=2, padx=50, sticky='E')
+        self.btn_sumit = tk.Button(self.mtk, text='增加', command=self.btn_sumit_handler).grid(row=4, column=2, sticky='W')
 
-        self.outputfn_txt = tk.Label(self.mtk, text='當前樂譜 : ').grid(row=5, column=0, pady=60)
+        self.outputfn_txt = tk.Label(self.mtk, text='當前樂譜 : ', wraplength=480)
+        self.outputfn_txt.grid(row=5, column=0, pady=60, columnspan=4, sticky='W')
 
-        self.btn_output = tk.Button(self.mtk, text='輸出檔案').grid(row=6, column=1)
+        self.btn_output = tk.Button(self.mtk, text='輸出檔案',command=self.btn_output_handler)
+        self.btn_output.grid(row=6, column=1)
+
+    def btn_sumit_handler(self):
+        if self.filename_inp.get() is '':
+            print('請打上輸出檔案名稱')
+            msb._show('錯誤', '請打上輸出檔案名稱')
+        elif self.chord.get() is '':
+            print('請輸入音符(0~7)')
+            msb._show('錯誤', '請輸入音符(0~7)')
+        elif self.sec.get() is '':
+            print('請輸入拍子數')
+            msb._show('錯誤', '請輸入拍子數')
+        else:
+            try:
+                chord_m = int(self.chord.get())
+            except ValueError:
+                msb._show('錯誤', '音符只能輸入數字')
+                return
+            try:
+                sec_m = int(self.sec.get())
+                if sec_m <= 0 or sec_m > 4:
+                    msb._show('錯誤', '拍數只能輸入數字(1~4)')
+                    return
+            except ValueError:
+                msb._show('錯誤', '拍數只能輸入數字(1~4)')
+                return
+            if chord_m < 0 or chord_m > 7:
+                msb._show('錯誤', '音符只能輸入(0~7)')
+                return
+            # print(self.outputfn_txt.cget('text'))
+
+            # print(self.count % 4)
+            if self.count % 4 is 0:
+                self.outputfn_txt.configure(text=str(self.outputfn_txt.cget('text')+'| '+self.generator.hzArr[chord_m][0] + '-' + str(sec_m) + ' '))
+            else:
+                self.outputfn_txt.configure(text=str(
+                    self.outputfn_txt.cget('text') + self.generator.hzArr[chord_m][0] + '-' + str(sec_m) + ' '))
+            self.count += 1
+            self.generator.spectrum.append([chord_m, sec_m])
+
+    def btn_delete_handler(self):
+        if len(self.outputfn_txt.cget('text')) is 7:
+            msb._show('錯誤', '譜是空的')
+        else:
+            del self.generator.spectrum[-1]
+            if self.count % 4 is 1:
+                strr = self.outputfn_txt.cget('text')
+                strr = strr[:-6]
+            else:
+                strr = self.outputfn_txt.cget('text')
+                strr = strr[:-4]
+            self.outputfn_txt.configure(text=strr)
+            self.count -= 1
+
+    def homeworkout(self):
+        data = []
+        data_low = []
+        spec = [[5, 1], [3, 1], [3, 2], [4, 1], [2, 1], [2, 2], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [5, 1], [5, 2], [5, 1], [3, 1], [3, 2], [4, 1], [2, 1], [2, 2],
+                [1, 1], [3, 1], [5, 1], [5, 1], [3, 4], [2, 1], [2, 1], [2, 1], [2, 1], [2, 1], [3, 1], [4, 2], [3, 1], [3, 1], [3, 1], [3, 1], [3, 1], [4, 1], [5, 2],
+                [5, 1], [3, 1], [3, 2], [4, 1], [2, 1], [2, 2], [1, 1], [3, 1], [5, 1], [5, 1], [1, 4]]
+        for i in spec:
+            data.extend(self.generator.generate_wave(i[0], i[1]))
+            data_low.extend(self.generator.lowgenerate_wave(i[0], i[1]))
+        for i, j in enumerate(data_low[:-25000]):
+            data[i+25000] += data_low[i]
+        data.extend(data_low[-25000:])
+        print(data_low)
+        self.generator.save_wave(np.array(data))
+        msb._show('成功', '輸出成功!')
+
+
+
+
+
 
 
 
--- Generator.py
+++ Generator.py
@@ -10,40 +10,43 @@ class SoundGenerator:
     def __init__(self, sample_rate, amplitude):
         self.sample_rate = sample_rate
         self.amplitude = amplitude
+        self.spectrum = []
+        self.hzArr = [
+            ['X', 1],
+            ['C', 262],
+            ['D', 294],
+            ['E', 330],
+            ['F', 349],
+            ['G', 392],
+            ['A', 440],
+            ['B', 494]
+        ]
+
+        self.lowhzArr = [
+            ['X', 1],
+            ['C', 65],
+            ['D', 76],
+            ['E', 82],
+            ['F', 87],
+            ['G', 98],
+            ['A', 110],
+            ['B', 124]
+        ]
 
     def print_properties(self):
         print(self.sample_rate, self.amplitude)
 
-    def generate_wave(self, frequency, sec):
-        wave_data = self.amplitude * np.sin(2 * np.pi * frequency / self.sample_rate * np.arange(sec * self.sample_rate)).astype(np.float32)
+    def generate_wave(self, chord, sec):
+        wave_data = self.amplitude * np.sin(2 * np.pi * self.hzArr[chord][1] / self.sample_rate * np.arange(sec * 0.5 * self.sample_rate)).astype(np.float32)
         return wave_data
 
-    def play_wave(self, wave_data):
-        # 寫入音檔
-        p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paFloat32,
-                        channels=1,
-                        rate=self.sample_rate,
-                        output=True)
-
-        # 關閉 Stream
-        stream.stop_stream()
-        stream.close()
-
-        # 關閉 Pyaudio
-        p.terminate()
+    def lowgenerate_wave(self, chord, sec):
+        wave_data = 3 * np.sin(2 * np.pi * self.lowhzArr[chord][1] / self.sample_rate * np.arange(sec * 0.5 * self.sample_rate)).astype(np.float32)
+        return wave_data
 
     def save_wave(self, wave_data):
         # 寫入 Wave 檔案
-        wf = wave.open('record.wav', 'wb')
-        wf.setnchannels(1)
-        wf.setsampwidth(pyaudio.paFloat32)
-        wf.setframerate(self.sample_rate*22)
-        wf.writeframes(b''.join(wave_data))
-        print(self.sample_rate)
-        wf.close()
-
-        wwf.write('kappa.wav', self.sample_rate, wave_data)
+        wwf.write('HomeWork.wav', self.sample_rate, wave_data)
 
 
 
